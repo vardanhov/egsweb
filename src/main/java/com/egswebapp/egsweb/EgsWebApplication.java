d409265c1dccfafb77855ebff31502f8690ece23
Оганнисян Вардан <v.ogannisyan@razvitie-mo.ru>
2020-11-26 23:29:59 +0300

    kdso

--- src/main/java/com/egswebapp/egsweb/EgsWebApplication.java
+++ src/main/java/com/egswebapp/egsweb/EgsWebApplication.java
@@ -14,7 +14,7 @@ public class EgsWebApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(EgsWebApplication.class, args);
 
-//hello
+//hello world
     }
 
 }
